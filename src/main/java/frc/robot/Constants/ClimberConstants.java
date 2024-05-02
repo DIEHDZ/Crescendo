34408695b12be6e708cfbaf5d3f79b32b429611b
Diego <diego.hdez.r.2016@gmail.com>
2024-05-02 10:50:37 -0600

    lol

---
+++ src/main/java/frc/robot/Constants/ClimberConstants.java
@@ -0,0 +1,9 @@
+package frc.robot.Constants;
+
+public class ClimberConstants {
+    public static final double BOTTOM_BOUNDARY = 0.0; // meters
+    public static final double TOP_BOUNDARY = 4.75; // meters
+    public static final double GEAR_RATIO = 48.0; // hypothetical gear ratio
+    public static final double SPEED = 0.8; // speed factor
+}
+
